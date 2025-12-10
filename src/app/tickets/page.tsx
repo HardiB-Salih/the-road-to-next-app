a2c0644d135b7ac585b869fa9c6d0729343f223a
hardib.salih <hardib.salih@gmail.com>
2025-12-11 00:52:27 +0300

    67. Streaming

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,20 +1,17 @@
+import { Suspense } from "react";
 import Heading from "@/components/heading";
-import TicketItem from "@/features/ticket/components/ticket-item";
-import { getTickets } from "@/features/ticket/queries/get-tickets";
+import TicketList from "@/features/ticket/components/ticket-list";
 
-const TicketsPage = async () => {
-  const tickets = await getTickets();
+const TicketsPage = () => {
   return (
     <div className="flex flex-1 flex-col gap-y-8">
       <Heading
         title="Tickets Page"
         description=" All Your Tickets at one place"
       />
-      <div className="animate-fade-in-from-top flex flex-1 flex-col items-center gap-y-4">
-        {tickets.map((ticket) => (
-          <TicketItem key={ticket.id} ticket={ticket} />
-        ))}
-      </div>
+      <Suspense>
+        <TicketList />
+      </Suspense>
     </div>
   );
 };
